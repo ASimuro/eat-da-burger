50f8786a082b3551cd1041f9a37e1d3a4a9ce88f
Alex Simuro <aasimuro12@gmail.com>
2019-11-01 17:05:20 -0400

    orm, controller file, model(burger.js), style of website, index handlebars, server

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,8 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id Int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(127) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-	date TIMESTAMP DEFAULT now(),
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,5 @@ INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
 INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
 INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
 INSERT INTO burgers (burger_name) VALUES ('Steak Burger');
+INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
+INSERT INTO burgers (burger_name) VALUES ('Lamb Burger');
